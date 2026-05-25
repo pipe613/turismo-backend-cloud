f2b194e3ed6c845a0c29259d98562f3d9d7063ec
Felipe <felipearevalo510@gmail.com>
2026-05-25 00:38:09 -0400

    Arreglo serializer2

--- core/serializers.py
+++ core/serializers.py
@@ -19,24 +19,6 @@ class DisponibilidadSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ReservaSerializer(serializers.ModelSerializer):
-    # Definimos 'fecha' como un campo extra que mapea a 'fecha_reserva'
-    fecha = serializers.DateField(source='fecha_reserva', write_only=True, required=False)
-
     class Meta:
         model = Reserva
-        fields = ['id', 'user', 'tour', 'fecha_reserva', 'fecha']
-        # Hacemos que fecha_reserva no sea obligatorio en la validación inicial
-        extra_kwargs = {
-            'fecha_reserva': {'required': False},
-            'user': {'required': False}
-        }
-
-    def validate(self, data):
-        # Si la app envía 'fecha_reserva', la usamos. Si no, usamos 'fecha'.
-        if not data.get('fecha_reserva') and data.get('fecha'):
-            data['fecha_reserva'] = data['fecha']
-        
-        if not data.get('fecha_reserva'):
-            raise serializers.ValidationError({"fecha": "Este campo es requerido."})
-            
-        return data
+        fields = ['id', 'tour', 'fecha_reserva', 'user']
